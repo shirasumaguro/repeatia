fc1c850e66590e8c944f66dfb119941ea96226c8
Yuta Chiba <shirasumaguro@gmail.com>
2024-10-07 12:02:56 +0100

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -184,8 +184,8 @@ class _MyHomePageState extends State<MyHomePage> {
           });
         },
         localeId: _selectedLanguage, // 言語コードを指定
-        listenFor: Duration(seconds: 5), // リスニングの時間を指定
-        pauseFor: Duration(seconds: 2), // 無音検出後の一時停止
+        listenFor: Duration(seconds: 8), // リスニングの時間を指定
+        pauseFor: Duration(seconds: 1), // 無音検出後の一時停止
         partialResults: true, // 部分的な結果も受け取る
       );
     } else {
